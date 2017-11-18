9c9256d670f04872b2ed769748b7cf8e359d507a
atsuo1203 <dondonkitemasu@gmail.com>
2017-11-18 22:47:23 +0900

    ブタの情報取得完了

--- app/request.py
+++ app/request.py
@@ -8,8 +8,12 @@ def result_print():
     name = parse.quote_plus('ブタ', encoding='utf-8')
     html = request.urlopen('https://ja.wikipedia.org/wiki/' + name).read()
     soup = BeautifulSoup(html, 'html.parser')
-    table = soup.find('table', class_='borderless')
-    return table
+    trs = soup.find('table', class_='borderless').find_all('tr')
+    words = [td.find_all('td')[2].find('a') for td in trs]
+    last_word = trs[-1].find('b').string
+    result_list = [a.string for a in words if a is not None]
+    result_list.append(last_word)
+    return result_list
 
 
 print(result_print())
