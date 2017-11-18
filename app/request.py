e0f384f176079555c145a682d45a5b087fd8081a
atsuo1203 <dondonkitemasu@gmail.com>
2017-11-19 02:01:57 +0900

    エラーチェック2

--- app/request.py
+++ app/request.py
@@ -1,16 +1,24 @@
 from urllib import parse
 from urllib import request
+from urllib import error
 
 from bs4 import BeautifulSoup
 
 
 def scientific_name(wiki_name):
     name = parse.quote_plus(wiki_name, encoding='utf-8')
-    html = request.urlopen('https://ja.wikipedia.org/wiki/' + name).read()
+    # ページが存在しない時のエラー
+    try:
+        html = request.urlopen('https://ja.wikipedia.org/wiki/' + name).read()
+    except error.HTTPError:
+        return ['そのページは存在しません', 'カタカナで試してみてください']
+
     soup = BeautifulSoup(html, 'html.parser')
     table = soup.find('table', class_='borderless')
+
     if table is None:
-        return ['分類が載っていません']
+        return ['分類が載っていません', 'カタカナで試してみてください', 'それでもダメなら分類が追加されるまで待ちましょう']
+
     trs = table.find_all('tr')
     words = [td.find_all('td')[2].find('a') for td in trs]
     last_word = trs[-1].find('b').string
