3e4f55cfcea55ebf0e0ce0eb16aad594dc57452d
atsuo1203 <dondonkitemasu@gmail.com>
2017-11-26 00:25:46 +0900

    get method追加

--- app/api.py
+++ app/api.py
@@ -7,12 +7,14 @@ from app import app
 from app.request import scientific_name
 
 
-@app.route('/', methods=['POST'])
-@cross_origin()
+@app.route('/', methods=['GET', 'POST'])
+# @cross_origin()
 def show_wiki_name():
-    data = json.loads(request.data.decode('utf-8'))
-    wiki_name = data['name']
-    word_list = scientific_name(wiki_name)
-    response = {'name': word_list}
-    return make_response(jsonify(response))
+    if request.method == 'POST':
+        data = json.loads(request.data.decode('utf-8'))
+        wiki_name = data['name']
+        word_list = scientific_name(wiki_name)
+        response = {'name': word_list}
+        return make_response(jsonify(response))
 
+    return '値を送ってください'
