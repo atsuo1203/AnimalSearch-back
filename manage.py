d4511204a18423e649a54fdb2d0baffaf69a6357
atsuo1203 <dondonkitemasu@gmail.com>
2017-11-26 02:30:22 +0900

    helloをmanageに変更

--- manage.py
+++ manage.py
@@ -1,2 +1,26 @@
-from animal_search import app
-app.run()
+import json
+
+from flask import Flask
+from flask_cors import CORS
+from flask import request, make_response, jsonify
+
+from animal_search.request import scientific_name
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def show_wiki_name():
+    if request.method == 'POST':
+        data = json.loads(request.data.decode('utf-8'))
+        wiki_name = data['name']
+        word_list = scientific_name(wiki_name)
+        response = {'name': word_list}
+        return make_response(jsonify(response))
+
+    return '値を送ってください'
+
+
+if __name__ == '__main__':
+    app.run()
